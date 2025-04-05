b3fc9adae625c3586d02c50031a60c4309dab4b0
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2025-04-05 12:14:38 -0400

    enum exchg_id in struct exchg_limit_order

--- include/exchg/orders.h
+++ include/exchg/orders.h
@@ -5,9 +5,10 @@
 #define EXCHG_ORDERS_H
 
 #include "exchg/decimal.h"
+#include "exchg/exchanges.h"
 
 struct exchg_limit_order {
-        int exchange_id;
+        enum exchg_id exchange_id;
         int64_t update_micros;
         decimal_t price;
         decimal_t net_price;
@@ -15,7 +16,7 @@ struct exchg_limit_order {
 };
 
 struct exchg_l2_update {
-        int exchange_id;
+        enum exchg_id exchange_id;
         int num_bids;
         struct exchg_limit_order *bids;
         int num_asks;
